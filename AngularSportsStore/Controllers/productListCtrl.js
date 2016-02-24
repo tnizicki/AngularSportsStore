27efe739652654ed07af7f315f4b41f9c1a55f6a
tnizicki <tomek.nizicki@gmail.com>
2016-02-24 13:41:07 +0100

    Pagination, categories

---
+++ AngularSportsStore/Controllers/productListCtrl.js
@@ -0,0 +1,34 @@
+﻿angular.module("sportsStore")
+    .constant("categoryActiveClass", "btn-primary")
+    .constant("pageSize", 3)
+    .controller("productListCtrl", function ($scope, $filter, categoryActiveClass, pageSize) {
+        var selectedCategory = null;
+
+        $scope.selectCategory = function (newCategory) {
+            selectedCategory = newCategory;
+            $scope.selectedPage = 1;
+        }
+
+        $scope.categoryFilterFn = function (product) {
+            return selectedCategory == null || product.category == selectedCategory;
+        }
+
+        $scope.getCategoryActiveClass = function (category) {
+            if (category == selectedCategory) {
+                return categoryActiveClass;
+            } else {
+                return "";
+            }
+        }
+
+        $scope.pageSize = pageSize;
+        $scope.selectedPage = 1;
+
+        $scope.getPageClass = function (page) {
+            if (page == selectedPage) {
+                return categoryActiveClass;
+            } else {
+                return "";
+            }
+        }
+    });
